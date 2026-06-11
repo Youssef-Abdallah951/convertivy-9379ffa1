c61d02718cb88ec6b5b48b37cf0497480dfec6e2
Youssef-Abdallah951 <yb109324@gmail.com>
2026-06-11 14:16:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/https://github.com/Youssef-Abdallah951/convertivy.git/",
+
   server: {
     host: "::",
     port: 8080,
